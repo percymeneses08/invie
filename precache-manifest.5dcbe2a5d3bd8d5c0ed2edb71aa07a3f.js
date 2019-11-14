1105becac47c3cfe8a387142076f4e8663616a35
percymeneses08 <percymeneses08@gmail.com>
2019-11-14 17:53:09 -0500

    Updates

---
+++ precache-manifest.5dcbe2a5d3bd8d5c0ed2edb71aa07a3f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb1051b4024bc0cb1b95d978f07eb3b",
+    "url": "/invie/index.html"
+  },
+  {
+    "revision": "04411d848eb44c124525",
+    "url": "/invie/static/css/main.7beb98a3.chunk.css"
+  },
+  {
+    "revision": "08c7b4dc891e77f16700",
+    "url": "/invie/static/js/2.140b072f.chunk.js"
+  },
+  {
+    "revision": "04411d848eb44c124525",
+    "url": "/invie/static/js/main.a5fe6740.chunk.js"
+  },
+  {
+    "revision": "58c9dd1eb628dfd5086e",
+    "url": "/invie/static/js/runtime-main.e8ef22b1.js"
+  },
+  {
+    "revision": "cba5fb6368a2ff90d62cc89f9ffefc58",
+    "url": "/invie/static/media/background.cba5fb63.png"
+  },
+  {
+    "revision": "313656d46d6613b2d25f50a785361e1c",
+    "url": "/invie/static/media/corazon.313656d4.png"
+  },
+  {
+    "revision": "ba2e415e817351fb402d07afd2be5ceb",
+    "url": "/invie/static/media/easter-a.ba2e415e.png"
+  },
+  {
+    "revision": "7d7a81dcaa54ebefe86d47f90c6bb589",
+    "url": "/invie/static/media/easter-b.7d7a81dc.png"
+  },
+  {
+    "revision": "ec9833fc9629bb65c737f60ba215f4d7",
+    "url": "/invie/static/media/invie-acustica.ec9833fc.png"
+  },
+  {
+    "revision": "6cdc30e08efc87a92750c94de20ddda4",
+    "url": "/invie/static/media/invie-classic.6cdc30e0.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/invie/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "70297162b2ac4eb6ac54a0ddd5f85bfe",
+    "url": "/invie/static/media/team-platzi-azul.70297162.jpg"
+  }
+]);
